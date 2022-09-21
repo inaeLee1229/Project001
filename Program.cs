c65bfdcedf551b35e2918e6047c5cd923f76b5b1
iiina <iiina@DESKTOP-A4R70HK>
2022-09-21 22:08:09 +0900

    초기 커밋.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project001
+{
+    class FahrenheitApp
+    {
+       public static void Main()
+        {
+            double C = 25.90;
+            double F=9/5*C+32;
+            Console.WriteLine(F);
+        }
+    }
+
+}
